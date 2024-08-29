9738ed4d949cacf0da636005f5659d198c7be03a
chrisda28 <chrisda28@gmail.com>
2024-08-29 18:26:36 -0400

    set up model evaluation flask route containing useful model metrics

--- app.py
+++ app.py
@@ -7,29 +7,38 @@ SP500 = "^GSPC"
 TIME_PERIOD = "10y"  # specifies how much data to collect,
 # must be one of ['1d', '5d', '1mo', '3mo', '6mo', '1y', '2y', '5y', '10y', 'ytd', 'max']
 
+
 @app.route("/")
 def home():
-    """render the home page"""
+    """render the home page/introduction"""
     return render_template(template_name_or_list='index.html', title='Stock Daily Return Direction Predicting',
                            header='',)
 
 
 @app.route("/model")
 def model():
-    df, missing_count = get_daily_stock_data(ticker=SP500, time_period=TIME_PERIOD)
+    """Run model and display the prediction for future months"""
+    df, missing_count = get_daily_stock_data(ticker=SP500, time_period=TIME_PERIOD)  # DONT THINK I NEED THIS LINE OR THE ONE BELOW
     x_train, y_train, x_test, y_test = prep_data(df=df)
     predictions, accuracy, y_test = run_model(ticker=SP500, time_period=TIME_PERIOD)
-
-    return render_template(template_name_or_list='model.html')
-
+    # map the predictions to readable output instead of 1s and 0s !!!!!!!
+    return render_template(template_name_or_list='model.html', predictions=predictions)
 
 
 @app.route("/evaluate")
 def evaluate():
-
-
-
-    return render_template(template_name_or_list='evaluate.html')
+    """Show key model metrics"""
+    df, missing_count = get_daily_stock_data(ticker=SP500, time_period=TIME_PERIOD)
+    x_train, y_train, x_test, y_test = prep_data(df=df)
+    predictions, accuracy, y_test, lr_classifier = run_model(ticker=SP500, time_period=TIME_PERIOD)
+    accuracy = round(accuracy, 4)
+    confusion_img = plot_confusion_matrix(y_true=y_test, y_pred=predictions)
+    importance = feature_importance(model=lr_classifier, x_train=x_train)
+    importance_data = importance.to_dict('records')
+    return render_template(template_name_or_list='evaluate.html',
+                           confusion=confusion_img,
+                           accuracy=accuracy,
+                           importance=importance_data)
 
 
 
